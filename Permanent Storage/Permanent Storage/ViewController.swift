b1692046723dda0196b77559826f6b9ec4023e6a
Amin Rezapour <aminrezapour@gmail.com>
2016-03-16 14:42:41 -0700

    Add permanent storage

---
+++ Permanent Storage/Permanent Storage/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Permanent Storage
+//
+//  Created by Amin Rezapour on 3/16/16.
+//  Copyright © 2016 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSUserDefaults.standardUserDefaults().setObject("Amin", forKey: "name")
+        
+        let userName = NSUserDefaults.standardUserDefaults().objectForKey("name")! as! String
+        
+        print(userName)
+        
+        let arr = [1, 2, 3, 4]
+        
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "Array")
+        
+        let returnedArray = NSUserDefaults.standardUserDefaults().objectForKey("Array")! as! NSArray
+        
+        for x in returnedArray {
+            print(x)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
